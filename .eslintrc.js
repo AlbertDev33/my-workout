85d39672c1e7dde01357892b52af1348dca941f0
albert <albert.rocha@colab.re>
2022-06-27 16:30:15 -0300

    chore: project lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin', 
+    'eslint-plugin-import-helpers',
+    'prettier'
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -17,9 +21,43 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-param-reassign': ['error', { 'props': false }],
+    'lines-between-class-members': 'off',
+    'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+            'ts': 'never'
+        }
+    ],
+    'import-helpers/order-imports': [
+        'warn',
+        {
+            newlinesBetween: 'always',
+            groups: [
+                'module',
+                '/^@/',
+                [
+                    'parent',
+                    'sibling',
+                    'index'
+                ]
+            ],
+            alphabetize: {
+                'order': 'asc',
+                'ignoreCase': true
+            }
+        }
+    ],
+    settings: {
+        'import/resolver': {
+            'typescript': {}
+        }
+    },
   },
 };
