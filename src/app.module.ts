c891bd1e78e6329665c006154b62eef7c6393e20
albert <albert.rocha@colab.re>
2022-06-27 14:57:01 -0300

    feature: inject database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './configs/database.config';
+
+process.env.NODE_ENV = 'development';
+const env = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+      envFilePath: ['.env.local', !env ? '.env' : `.env.${env}`],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
