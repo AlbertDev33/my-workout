e18e58f40ee82662fc3b7dd024caade4855311f0
albert <albert.rocha@colab.re>
2022-06-28 10:55:17 -0300

    feature: muscle group migration

---
+++ src/migrations/1656376450302-createMuscleGroup.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createMuscleGroup1656376450302 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'muscle_group',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'workout_id',
+            type: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'exercise_name',
+            type: 'varchar',
+          },
+          {
+            name: 'amount',
+            type: 'numeric',
+          },
+          {
+            name: 'repetition',
+            type: 'numeric',
+          },
+          {
+            name: 'technique',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'load_weight',
+            type: 'numeric',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKWorkout',
+            referencedTableName: 'workout',
+            referencedColumnNames: ['id'],
+            columnNames: ['workout_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('muscle_group');
+  }
+}
